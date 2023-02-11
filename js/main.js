df46270228f14e7763771dfe81ec319ce90c4506
Anam Aadel <rruholislam1998@gmail.com>
2023-02-11 08:09:14 +0600

    porfolio

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// navigation start
+const nav_toggle = document.querySelector('.nav_toggle');
+const side_content = document.querySelector('.side_content');
+const side_nav = document.querySelector('#side_nav');
+console.log(side_nav)
+let inWidth = window.innerWidth;
+if(inWidth<= 992){
+  nav_toggle.addEventListener('click',function(){
+    this.classList.toggle('boom');
+    side_content.classList.toggle('boom');
+
+  })
+}
+const item_link = document.querySelectorAll('.item_link');
+
+for(item of item_link){
+  item.addEventListener('click',function(){
+    side_content.classList.remove("boom");
+  })
+}
+
+window.addEventListener('scroll',function(){
+  let inWidth = window.innerWidth;
+  if(inWidth < 992){
+    const scrolling = window.scrollY;
+    if(scrolling>300){
+      side_nav.classList.add('boom');
+    }else{
+      side_nav.classList.remove('boom');
+
+    }
+
+  }
+
+  
+})
+
+
+// navigation end
+
+
+// Typewriter start
+new Typewriter('.profession', {
+  strings: ['Aadel Banat','a Web Designer','a Freelanacer'],
+  autoStart: true,
+  loop: true,
+});
+// Typewriter end
+
